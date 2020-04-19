afeb747480de43b7568cce92e2688e2a675befc1
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-04-19 19:31:30 -0400

    Completed module 4

--- src/app.js
+++ src/app.js
@@ -3,14 +3,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 const { accounts, users, writeJSON } = require('./data.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({extended: false}));
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
     res.render('index', { 
@@ -19,57 +24,9 @@ app.get('/', (req, res) => {
     })
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', {
-        account: accounts.savings
-    })
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', {
-        account: accounts.checking
-    })
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', {
-        account: accounts.credit
-    })
-});
-
 app.get('/profile', (req, res) => {
     res.render('profile', {
         user: users[0]
-    })
-});
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', {
-        account: accounts.credit
-    });
-})
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance = accounts.credit.balance - req.body.amount;
-    accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-    accountsJSON = JSON.stringify(accounts);
-    writeJSON();
-    res.render('payment', {
-        message: "Payment Successful", 
-        account: accounts.credit
-    });
-})
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', {
-        message: "Transfer Completed"
     })
 });
 
